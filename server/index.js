120f80103d00a022913e0b4cf6c2c93024c9c648
Tahacoder14 <tahachhipa89@gmail.com>
2025-09-13 15:02:50 -0700

    resolved

--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server working on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
 });
 export default app;
