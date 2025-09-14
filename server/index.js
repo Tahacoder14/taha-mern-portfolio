f964a957359454511d62aabb74509955e0664612
Tahacoder14 <tahachhipa89@gmail.com>
2025-09-14 13:38:01 -0700

    fix(deploy)reset UI settings

--- server/index.js
+++ server/index.js
@@ -31,8 +31,10 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-app.use(cors({origin: true,credentials: true})); // <-- Use this for allowing all origins during development
-app.use(cors(corsOptions)); // <-- Use this in production with specific origins
+app.use(cors({
+  origin: ['https://taha-mern-portfolio.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
